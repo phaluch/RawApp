d8a57620c23e5cc57b031d81672b6bd325ee214d
Pedro Haluch <pedro.haluch@gmail.com>
2021-06-08 11:12:38 -0300

    Binding feito no homeFrag

---
+++ app/src/main/java/com/rawenterprises/rawapp/ui/fragment/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.rawenterprises.rawapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rawenterprises.rawapp.R
+import com.rawenterprises.rawapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.nossaHomeFragment = this
+        binding.lifecycleOwner = this
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+}
