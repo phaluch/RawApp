0b8b554b71e13800caf43c94fe47d58ffd27ebc8
Pedro Haluch <pedro.haluch@gmail.com>
2021-06-08 13:37:32 -0300

    Primeira tabela criada

--- app/src/main/java/com/rawenterprises/rawapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/rawenterprises/rawapp/ui/activity/MainActivity.kt
@@ -4,6 +4,9 @@ package com.rawenterprises.rawapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.rawenterprises.rawapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,3 +17,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+
+/** Entidades representam uma tabela no banco de dados local.
+ * Os atributos e características podem ser alterados
+ * através de anotações nos membros da classe. */
+@Entity
+data class Review(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+
+    var stars: Int = 0,
+    var texto: String? = null,
+
+    // Se eu quisesse colocar 'lá no BD' no nome dessa tabela como 'data_postagem', faria assim:
+    // @ColumnInfo(name = "data_postagem")
+    var data: String? = null
+)
