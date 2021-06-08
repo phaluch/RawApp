e4d60534069198107862d6db6a7e26cf8a60a2aa
Pedro Haluch <pedro.haluch@gmail.com>
2021-06-08 10:26:39 -0300

    Projeto em Branco

---
+++ app/src/main/java/com/rawenterprises/rawapp/MainActivity.kt
@@ -0,0 +1,13 @@
+// First push dev
+
+package com.rawenterprises.rawapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
