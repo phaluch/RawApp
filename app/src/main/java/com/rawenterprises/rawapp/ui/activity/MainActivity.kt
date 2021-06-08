b29ab32e8e03b5f7eeb3e54faae0608b8b32ad3f
Samuel Rocha <samuelsilv.rocha@gmail.com>
2021-06-08 11:00:49 -0300

    Move main activity

--- app/src/main/java/com/rawenterprises/rawapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/rawenterprises/rawapp/ui/activity/MainActivity.kt
@@ -1,9 +1,10 @@
 // First push dev
 
-package com.rawenterprises.rawapp
+package com.rawenterprises.rawapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rawenterprises.rawapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
