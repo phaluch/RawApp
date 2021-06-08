22a970ebbfc3c0efcfd7ce1c3d9fd80e5179309a
Samuel Rocha <samuelsilv.rocha@gmail.com>
2021-06-08 16:18:10 -0300

    Teste Parse

--- app/src/main/java/com/rawenterprises/rawapp/RawApp.kt
+++ app/src/main/java/com/rawenterprises/rawapp/RawApp.kt
@@ -1,8 +1,22 @@
 package com.rawenterprises.rawapp
 
 import android.app.Application
+import com.parse.Parse
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class RawApp : Application() {
+
+    // Initializes Parse SDK as soon as the application is created
+    override fun onCreate() {
+        super.onCreate()
+        Parse.initialize(
+            Parse.Configuration.Builder(this)
+                .applicationId("hYqvNNWshu88Ch6ItwORC4Prm1otQVLEgbxGh2bB")
+                .clientKey("kl5lorpvdyoLj6i371AVVakqV4psFkw3drSreuNH")
+                .server("https://parseapi.back4app.com")
+                .build()
+        )
+    }
+
 }
