0cdb14d7bcc93bb59450be4e094446f6f855a9ec
Samuel Rocha <samuelsilv.rocha@gmail.com>
2021-06-09 23:17:50 -0300

    Adicionar o onboarding e modificacoes nos fragments

--- app/src/main/java/com/rawenterprises/rawapp/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/rawenterprises/rawapp/ui/fragment/LoginFragment.kt
@@ -1,14 +1,16 @@
 package com.rawenterprises.rawapp.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
-import com.rawenterprises.rawapp.R
-import com.rawenterprises.rawapp.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.rawenterprises.rawapp.databinding.FragmentLoginBinding
+import com.rawenterprises.rawapp.ui.activity.AppActivity
 import com.rawenterprises.rawapp.viewmodel.RawViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -28,6 +30,28 @@ class LoginFragment : Fragment() {
 
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btLogin.setOnClickListener {
+            val email = binding.txEmailLogin.text.toString()
+            val password = binding.txEmailSenha.text.toString()
+            val autenticacao = FirebaseAuth.getInstance()
+
+            val taskDeLogin = autenticacao.signInWithEmailAndPassword(email, password)
+
+            taskDeLogin.addOnCompleteListener { resultado ->
+                if(resultado.isSuccessful) {
+                    val intencaoDeChamada = Intent(activity, AppActivity::class.java)
+                    activity?.startActivity(intencaoDeChamada)
+                } else {
+                    Toast.makeText(activity, "Um dos campos está incorreto.", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
     }
 
 }
