bb7531ed5a019b43b83038410094a03ab0372558
Samuel Rocha <samuelsilv.rocha@gmail.com>
2021-06-13 00:22:04 -0300

    Auto stash before merge of "dev" and "origin/pedro"

---
+++ app/src/main/java/com/rawenterprises/rawapp/ui/fragment/MapsFragment.kt
@@ -0,0 +1,31 @@
+package com.rawenterprises.rawapp.ui.fragment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rawenterprises.rawapp.R
+import com.rawenterprises.rawapp.databinding.FragmentHomeBinding
+import com.rawenterprises.rawapp.databinding.FragmentMapsBinding
+
+
+class MapsFragment : Fragment() {
+
+    private lateinit var binding: FragmentMapsBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentMapsBinding.inflate(inflater, container, false)
+        binding.mapsFragment= this
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+
+}
